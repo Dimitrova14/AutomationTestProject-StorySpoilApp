62d28ae353c28c7e28e830fb8fa6533bd71fba84
Ванина Димитрова <you@example.com>
2024-11-17 14:11:41 +0200

    Added folders for Pages & Tests

---
+++ Pages/LoginPage.cs
@@ -0,0 +1,21 @@
+﻿using OpenQA.Selenium;
+
+namespace StorySpoilAppTests.Pages
+{
+    public class LoginPage : BasePage
+    {
+        public LoginPage(IWebDriver driver) : base(driver)
+        {
+        }
+
+        private readonly string Url = BaseUrl + "/User/Login";
+
+        //elements
+        private readonly By usernameField = By.Id("username");
+        private readonly By passwordField = By.Id("password");
+        private readonly By loginBtn = By.CssSelector("button[type='submit']");
+
+        //error msgs
+        //methods
+    }
+}
---
+++ Tests/BaseTest.cs
@@ -0,0 +1,48 @@
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using StorySpoilAppTests.Pages;
+
+namespace StorySpoilAppTests.Tests
+{
+    public class BaseTest
+    {
+        protected IWebDriver driver;
+        protected RegisterPage registerPage;
+        protected LoginPage loginPage;
+        protected HomePage homePage;
+        protected CreateSpoilerPage createSpoilerPage;
+        protected EditSpoilerPage editSpoilerPage;
+
+        [SetUp]
+        public void Setup()
+        {
+            var options = new ChromeOptions();
+            options.AddUserProfilePreference("profile.password_manager_enabled", false);
+            driver = new ChromeDriver(options);
+            driver.Manage().Window.Maximize();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+
+            registerPage = new RegisterPage();
+            loginPage = new LoginPage();
+            homePage = new HomePage();
+            createSpoilerPage = new CreateSpoilerPage();
+            editSpoilerPage = new EditSpoilerPage();
+        }
+
+        [TearDown] 
+        public void TearDown() 
+        {
+            if (driver != null)
+            {
+                driver.Quit();
+                driver.Dispose();
+            }
+        }
+
+        protected void Login(string username, string password)
+        {
+            driver.Navigate().GoToUrl("");
+            LoginPage.LoginUser(username, password);
+        }
+    }
+}
